68c3b5647b7ca07728323e92aa30f9793b04631a
Your Name <ali3ajob@gmail.com>
2025-06-27 00:21:06 +0200

    feat: update Document and Home components; add page structure and title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Hero from '@/components/module/Hero';
+import About from '@/components/module/About';
+import WhyMinimal from '@/components/module/WhyMinimal';
+import Reviews from '@/components/module/Reviews';
+import Author from '@/components/module/Author';
+import Contact from '@/components/module/Contact';
 
 export default function Home() {
   return (
     <div>
-      The minimal Trader
+      <Hero />
+      <About />
+      <WhyMinimal />
+      <Reviews />
+      <Author />
+      <Contact />
     </div>
   );
 }
