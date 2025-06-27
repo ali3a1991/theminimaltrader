676d484bbcd04b401a604953d4fb8f4d9d2d2a98
Your Name <ali3ajob@gmail.com>
2025-06-27 08:18:09 +0200

    fix: correct component name casing for About and Contact components

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-export default function contact() {
+export default function Contact() {
   const [formData, setFormData] = useState({
     firstName: '',
     lastName: '',
