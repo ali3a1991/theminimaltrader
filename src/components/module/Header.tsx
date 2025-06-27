1634af983fd11369bba4e14b7060b4ba87f6ce00
Your Name <ali3ajob@gmail.com>
2025-06-27 13:16:51 +0200

    feat: update Header component styling and add logo font; enhance global styles with Playfair Display font

--- src/components/module/Header.tsx
+++ src/components/module/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between p-4 bg-gray-800 text-white h-10">
-      <h1 className="text-2xl font-bold">navbar</h1>
+    <div className="flex items-center justify-between p-4 fixed">
+      <h1 className="text-2xl font-bold logo">The Minimal Trader</h1>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,12 @@ import WhyMinimal from '@/components/module/WhyMinimal';
 import Reviews from '@/components/module/Reviews';
 import Author from '@/components/module/Author';
 import Contact from '@/components/module/Contact';
+import Header from '@/components/module/Header';
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <Hero />
       <About />
       <WhyMinimal />
